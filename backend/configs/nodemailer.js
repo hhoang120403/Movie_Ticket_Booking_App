e3d65bbeb35e3e4afdd0dae82645d428bfa01599
hhoang_1204 <hhoang12042003@gmail.com>
2025-08-21 21:33:24 +0700

    Add Brevo Api Test

--- backend/configs/nodemailer.js
+++ backend/configs/nodemailer.js
@@ -1,21 +1,41 @@
-import nodemailer from 'nodemailer';
+// import nodemailer from 'nodemailer';
+import SibApiV3Sdk from 'sib-api-v3-sdk';
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp-relay.brevo.com',
-  port: 587,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp-relay.brevo.com',
+//   port: 587,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
+// const sendEmail = async ({ to, subject, body }) => {
+//   const response = await transporter.sendMail({
+//     from: process.env.SENDER_EMAIL,
+//     to,
+//     subject,
+//     html: body,
+//   });
+
+//   return response;
+// };
+
+let defaultClient = SibApiV3Sdk.ApiClient.instance;
+let apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+let apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const sendEmail = async ({ to, subject, body }) => {
-  const response = await transporter.sendMail({
-    from: process.env.SENDER_EMAIL,
-    to,
-    subject,
-    html: body,
-  });
+  let sendSmtpEmail = {
+    sender: { email: process.env.SENDER_EMAIL, name: 'Netflix Movie App' },
+    to: [{ email: to }],
+    subject: subject,
+    htmlContent: body,
+  };
+
+  const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
 
   return response;
 };
